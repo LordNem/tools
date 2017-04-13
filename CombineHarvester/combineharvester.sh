22dddf58e459caf61755dddfb8ea530221c5280d
LordNem <lordn3mesis@hotmail.com>
2017-04-13 04:18:18 +0100

    Update combineharvester.sh

--- CombineHarvester/combineharvester.sh
+++ CombineHarvester/combineharvester.sh
@@ -197,5 +197,5 @@ cat $tmpdir/shared-emails | sort | uniq > nothuman.$domain
 
 
 # *molotov*
-cat $tmpdir/humans.csv |cut -d "," -f 3 >> $tmpdir/Email_HIBP
+cat humans.csv |cut -d "," -f 1,3 | awk '$1=$1' FS="," OFS="." |grep -v no. >> $tmpdir/Email_HIBP
 echo -e ${green} "[+]" ${white} "Output files saved to" $tmpdir
