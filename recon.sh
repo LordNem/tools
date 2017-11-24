7773f7162f3faea0bc595486ba057831b013e1ed
LordNem <lordn3mesis@hotmail.com>
2017-11-24 12:56:26 +0000

    Create recon.sh

---
+++ recon.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+printf "\33c"
+red='\033[0;31m'
+green='\033[0;32m'
+white='\033[1;37m'
+blue='\033[0;34m'
+yellow='\033[1;33m'
+
+
+
+if [ -z $2 ]; then
+
+echo -e ${red} "[X]" ${white} "Usage: Enter Domain and output path"
+
+        exit 1;
+
+fi
+
+CWD="$(pwd)"
+echo -e ${green} "[+]" ${white} "Loading DNSRecon" ${yellow}
+cat << "EOF"
+
+______ _   _  ___________                     
+|  _  \ \ | |/  ___| ___ \                    
+| | | |  \| |\ `--.| |_/ /___  ___ ___  _ __  
+| | | | . ` | `--. \    // _ \/ __/ _ \| '_ \ 
+| |/ /| |\  |/\__/ / |\ \  __/ (_| (_) | | | |
+|___/ \_| \_/\____/\_| \_\___|\___\___/|_| |_|
+             
+EOF
+
+cd /root/tools/dnsrecon
+echo -e ${blue} "[-]" ${white} "A output will be found here " $2
+./dnsrecon.py -n 8.8.8.8 --domain $1 -t axfr -x $2/$1
+
+
+cd /root/tools/Domain-Mail-Check
+echo  -e ${green} "[+]" ${white} "Checking for SPF and DMARC" ${yellow}
+cat << "EOF"
+  _____                   _____  __  __  _____ 
+ |  __ \                 |  __ \|  \/  |/ ____|
+ | |__) |   _ _ __ ______| |  | | \  / | |     
+ |  _  / | | | '_ \______| |  | | |\/| | |     
+ | | \ \ |_| | | | |     | |__| | |  | | |____ 
+ |_|  \_\__,_|_| |_|     |_____/|_|  |_|\_____|
+EOF
+echo -e ${white} 
+python Run-DMC -d $1
+
+echo  -e ${blue} "[-]" ${white} "An XML output is also in the Domain-Mail-Check Folder"
+
+cd /root/tools/HaveTheyBeenPwned
+
+echo -e ${green} "[+]" ${white} "Extracting Email Addresses for HIBP" ${yellow}
+cat $2/humans.csv  | cut -d , -f 1,3 |cut -d , -f 2|sort -u |grep -v "found@" > $2/Emails.txt
+cat << "EOF"
+
+cd ..
+echo  -e ${green} "[+]" ${white} "Complete"
