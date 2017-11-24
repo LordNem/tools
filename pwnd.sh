b1d9cf08eadb4354b1caeb3ea1746ea7a7f68ef1
LordNem <lordn3mesis@hotmail.com>
2017-11-24 12:57:47 +0000

    Update pwnd.sh

--- pwnd.sh
+++ pwnd.sh
@@ -30,37 +30,6 @@ EOF
 cd /root/tools/CombineHarvester
 ./combineharvester.sh "$1" "$2"
 cd ..
-echo -e ${green} "[+]" ${white} "Loading DNSRecon" ${yellow}
-cat << "EOF"
-
-______ _   _  ___________                     
-|  _  \ \ | |/  ___| ___ \                    
-| | | |  \| |\ `--.| |_/ /___  ___ ___  _ __  
-| | | | . ` | `--. \    // _ \/ __/ _ \| '_ \ 
-| |/ /| |\  |/\__/ / |\ \  __/ (_| (_) | | | |
-|___/ \_| \_/\____/\_| \_\___|\___\___/|_| |_|
-             
-EOF
-
-cd /root/tools/dnsrecon
-echo -e ${blue} "[-]" ${white} "A output will be found here " $2
-./dnsrecon.py -n 8.8.8.8 --domain $1 -t axfr -x $2/$1
-
-
-cd /root/tools/Domain-Mail-Check
-echo  -e ${green} "[+]" ${white} "Checking for SPF and DMARC" ${yellow}
-cat << "EOF"
-  _____                   _____  __  __  _____ 
- |  __ \                 |  __ \|  \/  |/ ____|
- | |__) |   _ _ __ ______| |  | | \  / | |     
- |  _  / | | | '_ \______| |  | | |\/| | |     
- | | \ \ |_| | | | |     | |__| | |  | | |____ 
- |_|  \_\__,_|_| |_|     |_____/|_|  |_|\_____|
-EOF
-echo -e ${white} 
-python Run-DMC -d $1
-
-echo  -e ${blue} "[-]" ${white} "An XML output is also in the Domain-Mail-Check Folder"
 
 cd /root/tools/HaveTheyBeenPwned
 
